dec215eecb5817b10653c4ed07fb4032896284cd
Tobias Sjösten <tobias.sjosten@gmail.com>
2010-10-12 00:15:25 +0200

    Generated plugin stub with sfTaskExtraPlugin

---
+++ config/sfFlattrPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * sfFlattrPlugin configuration.
+ * 
+ * @package     sfFlattrPlugin
+ * @subpackage  config
+ * @author      Tobias Sjösten
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class sfFlattrPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
