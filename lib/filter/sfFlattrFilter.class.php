aa5560b6e34f037e1a31b61d1dcd2ab395a917df
Tobias Sjösten <tobias.sjosten@gmail.com>
2011-03-27 20:31:48 +0200

    Updated to v0.6 of the JS loader

--- lib/filter/sfFlattrFilter.class.php
+++ lib/filter/sfFlattrFilter.class.php
@@ -26,17 +26,17 @@ class sfFlattrFilter extends sfFilter
 
     $loader = <<<LOADER
 <script type="text/javascript">
-//<![CDATA[
+/* <![CDATA[ */
     (function() {
         var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
 
         s.type = 'text/javascript';
         s.async = true;
-        s.src = 'http://api.flattr.com/js/0.5.0/load.js?mode=auto';
+        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
 
         t.parentNode.insertBefore(s, t);
     })();
-//]]>
+/* ]]> */
 </script>
 LOADER;
 
